dd05236f4a56b613ec8626f5e0370a96b7bd52f5
Marlon_Figueroa <marlon.f.1993@gmail.com>
2023-10-21 10:32:09 -0600

    first commit

---
+++ src/main/java/com/mballem/curso/security/repository/PerfilRepository.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.security.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mballem.curso.security.domain.Perfil;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+	
+	Optional<Perfil> findByDesc(String desc);
+
+}
---
+++ src/main/java/com/mballem/curso/security/web/controller/PacienteValidator.java
@@ -0,0 +1,40 @@
+package com.mballem.curso.security.web.controller;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import com.mballem.curso.security.domain.Paciente;
+import com.mballem.curso.security.service.PacienteService;
+
+public class PacienteValidator implements Validator {
+
+    PacienteService service;
+
+    public PacienteValidator(PacienteService service) {
+        this.service = service;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+
+        return Paciente.class.equals(clazz);
+
+    }
+
+    @Override
+    public void validate(Object object, Errors errors) {
+
+        Paciente entity = (Paciente) object;
+
+        if (entity.getId() != null) {
+
+            if (1 != 0) {
+
+                errors.rejectValue("nome", "Existe.nome");
+
+            }
+
+        }
+
+    }
+}
