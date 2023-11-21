726da7fc745ed02e369c073681acbb637487e98e
Marlon_Figueroa <marlon.f.1993@gmail.com>
2023-11-21 14:12:29 -0600

    Addicion de correos

--- src/main/java/org/app/security/service/EmailService.java
+++ src/main/java/org/app/security/service/EmailService.java
@@ -10,56 +10,58 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class EmailService {
 
-	@Autowired
-	private JavaMailSender mailSender;
+    @Autowired
+    private JavaMailSender mailSender;
 
-	@Autowired
-	private SpringTemplateEngine template;
+    @Autowired
+    private SpringTemplateEngine template;
+    
+    @Value("${spring.mail.username}")
+    String emisor;
 
-	public void enviarPedidoDeConfirmacaoDeCadastro(String destino, String codigo) throws MessagingException {
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-				"UTF-8");
+    public void enviarPedidoDeConfirmacaoDeCadastro(String destino, String codigo) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, "UTF-8");
 
-		Context context = new Context();
-		context.setVariable("titulo", "Bem vindo a clínica Spring Security");
-		context.setVariable("texto", "Precisamos que confirme seu cadastro, clicando no link abaixo");
-		context.setVariable("linkConfirmacao", "http://localhost:8080/u/confirmacao/cadastro?codigo=" + codigo);
+        Context context = new Context();
+        context.setVariable("titulo", "Bienvenido a DOCTORESA");
+        context.setVariable("texto", "Necesitamos que confirme su registro, haciendo click en el enlace de abajo");
+        context.setVariable("linkConfirmacao", "http://localhost:8080/u/confirmacao/cadastro?codigo=" + codigo);
 
-		String html = template.process("email/confirmacao", context);
-		helper.setTo(destino);
-		helper.setText(html, true);
-		helper.setSubject("Confirmacao de Cadastro");
-		helper.setFrom("nao-responder@clinica.com.br");
+        String html = template.process("email/confirmacao", context);
+        helper.setTo(destino);
+        helper.setText(html, true);
+        helper.setSubject("Confirmación de Registro");
+        helper.setFrom(emisor);
 
-		helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png"));
+        helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png"));
 
-		mailSender.send(message);
-	}
+        mailSender.send(message);
+    }
 
-	public void enviarPedidoRedefinicaoSenha(String destino, String verificador) throws MessagingException {
-		MimeMessage message = mailSender.createMimeMessage();
-		MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
-				"UTF-8");
+    public void enviarPedidoRedefinicaoSenha(String destino, String verificador) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+                "UTF-8");
 
-		Context context = new Context();
-		context.setVariable("titulo", "Redefinição de Senha");
-		context.setVariable("texto",
-				"Para redefinir sua senha use o código de verficação " + "quando exigido no formulário.");
-		context.setVariable("verificador", verificador);
+        Context context = new Context();
+        context.setVariable("titulo", "Redefinición de Clave");
+        context.setVariable("texto", "Para redefinir su clave user el codigo de verificación solicitado en el formulario.");
+        context.setVariable("verificador", verificador);
 
-		String html = template.process("email/confirmacao", context);
-		helper.setTo(destino);
-		helper.setText(html, true);
-		helper.setSubject("Redefinição de Senha");
-		helper.setFrom("no-replay@clinica.com.br");
+        String html = template.process("email/confirmacao", context);
+        helper.setTo(destino);
+        helper.setText(html, true);
+        helper.setSubject("Redefinición de Clave");
+        helper.setFrom(emisor);
 
-		helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png"));
+        helper.addInline("logo", new ClassPathResource("/static/image/spring-security.png"));
 
-		mailSender.send(message);
-	}
+        mailSender.send(message);
+    }
 }
